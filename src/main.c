2634ddea693bccaa55f695cbe65d05f9364818b1
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-04-09 17:20:03 +0800

    Change 10 seconds to 5 seconds.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <util/delay.h>
 
 int main(void) {
+  double delay5s = 5000;
+  double delay500ms = 500;
   int blinkCount = 1;
 
   // PORTD: Maps to Arduino digital pins 0 to 7.
@@ -15,13 +17,13 @@ int main(void) {
 
     if (blinkCount == 10) {
       // Turn on the light for 5 seconds.
-      _delay_ms(5000);
+      _delay_ms(delay5s);
 
       // Reset the state of blink count.
       blinkCount = 1;
     } else {
       // Turn the light for 500 milliseconds.
-      _delay_ms(500);
+      _delay_ms(delay500ms);
 
       // Increment the blink count state.
       blinkCount += 1;
@@ -31,7 +33,7 @@ int main(void) {
     PORTD &= ~(1 << PORTD3);
 
     // Turn off the LED for 500 milliseconds.
-    _delay_ms(500);
+    _delay_ms(delay500ms);
   }
 
   return 0;
